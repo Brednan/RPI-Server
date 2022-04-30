af6aedafcb1b859bebda381e3bf8bb645859a801
Brendan Caldwell <brendanshauncaldwell@gmail.com>
2022-04-29 18:05:50 -0700

    Debugging motor mechanisms

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from server import SocketServer
+# from server import SocketServer
+from motor_controls import MotorControls
 
-server = SocketServer('192.168.1.68', 65432)
-server.server_manager()
+# server = SocketServer('192.168.1.68', 65432)
+# server.server_manager()
+
+motors = MotorControls()
+motors.motor1.throttle(1.0) 
